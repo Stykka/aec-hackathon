5a85a977e93e0ed01449f1cf7d40f677ac96ca4c
Davide <davidercosta@gmail.com>
2023-03-04 21:35:43 +0100

    Add Object information table

--- speckleviewer/src/App.vue
+++ speckleviewer/src/App.vue
@@ -1,57 +1,98 @@
 <template>
-  <p>Speckle Viewer</p>
   <div
     class="flex flex-col mx-auto justify-between h-screen w-screen max-h-screen overflow-hidden"
   >
+    <h1>Speckle Viewer</h1>
     <label for="objectUrl">Object URL: </label>
     <input type="text" id="objectUrl" :value="objectUrl">
-    <h2>Select</h2>
-    <select v-model="selected">
+    <h2>Select material</h2>
+    <select v-model="selectedMaterial">
       <option disabled value="">Please select one</option>
       <option>A</option>
       <option>B</option>
       <option>C</option>
     </select>
-    <span>Selected: {{ selected }}</span>
+    <span>Selected material: {{ selectedMaterial }}</span>
+    <button @click="assignMaterial(selectedMaterial, $event)">Assign</button>
+    <table>
+      <th>Material
+        <td>{{ material }}</td>
+      </th>
+      <th>Volume
+        <td>{{ volume }}</td>
+      </th>
+      <th>ID
+        <td>{{ id }}</td>
+      </th>
+    </table>
   </div>
  
-  <div ref="canvas" class="canvas"></div>
+  <div ref="canvas" class="canvas p-2"></div>
 
 </template>
 
 <script lang="ts">
-import { SelectionEvent, Viewer, ViewerEvent } from "@speckle/viewer"
+import { SelectionEvent, SpeckleObject, Viewer, ViewerEvent } from "@speckle/viewer";
 import { onMounted, ref } from "vue";
 
 export default {
   data() {
     return {
-      selected: 'A'
+      selectedMaterial: 'A'
     }
   },
   setup() {
     const canvas = ref<HTMLElement>();
-    const objectUrl = ref("https://dev.stykka.com/streams/aa6b496028/objects/65a8fa46c214142b2ff426f886ffcaff");
+    const objectUrl = ref("https://dev.stykka.com/streams/d73703f251/objects/edfb08a14c58f39d0d9d23a5d675b299");
+    const material = ref()
+    const volume = ref()
+    const id = ref()
+
+    function assignMaterial(material, event) {
+      
+    }
 
     onMounted(async () => {
       const viewer = new Viewer(canvas.value!)
+      let allObjects = viewer.getDataTree()
+
+      let propertyData = viewer.getObjectProperties()
+      propertyData.forEach(function(item) {
+        if (item['key'] === 'volume') {
+          console.log("item: ", item)
+        }
+      })
+
       viewer.on(ViewerEvent.ObjectClicked, (selectionInfo: SelectionEvent) => {
         if (selectionInfo) {
+          let objectId = [selectionInfo.hits[0].object.id as string]
           // Object was clicked. Focus in on it
-          viewer.zoom([selectionInfo.hits[0].object.id as string])
-          viewer.highlightObjects([selectionInfo.hits[0].object.id as string])
+          viewer.zoom(objectId)
+          viewer.highlightObjects(objectId)
+          let obj = selectionInfo.hits[0].object
+          id.value = obj.id
+          volume.value = obj.volume
+          material.value = obj.material
         }
         else {
           // No object clicked. Restore focus to entire scene
           viewer.zoom()	
+          viewer.resetHighlight()
+          id.value = null
+          volume.value = null
+          material.value = null
         }
       })
       await viewer.loadObject(objectUrl.value)
     })
 
     return {
+      assignMaterial,
       canvas,
-      objectUrl
+      objectUrl,
+      volume,
+      id,
+      material
     }
   }
 }
